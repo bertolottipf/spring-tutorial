c22bbc610a2213021c02db3f56f74296048a518c
Marco Andreini <marco.andreini@gmail.com>
2016-02-25 15:01:38 +0100

    Conversione iniziale per spring boot tutorial.

---
+++ src/test/java/tutorial/spring/SpringTutorialApplicationTests.java
@@ -0,0 +1,18 @@
+package tutorial.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringTutorialApplication.class)
+@WebAppConfiguration
+public class SpringTutorialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
